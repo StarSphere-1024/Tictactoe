f45c87ede1931a1736e0c27e496eb853c5e62f48
StarSphere <starsphere@foxmail.com>
2024-08-23 12:44:05 +0800

    1.增加CMake配置文件 2.优化代码结构 3.修复程序bug

--- Src/main.c
+++ Src/main.c
@@ -1,10 +1,12 @@
 #include "tictactoe.h"
+#include <stdbool.h>
+#include <stdio.h>
 
 int main(){
 
     Tictactoe tictactoe;
     TictactoeFuc.init(&tictactoe);
-    while（ture）{
+    while(true){
         uint8_t selection=0;
         TictactoeFuc.start_play(&tictactoe);
         printf("[0] Play again\n[1] exit\n");
--- Src/player.c
+++ Src/player.c
@@ -7,7 +7,6 @@ const PlayerFuc_t PlayerFuc = {
  };
 static void init(Player *self, const char *name, unsigned char flag)
 {
-
     self->piece_queue = Queue_Fuc.createQueue();
     self->name = name;
     self->flag = flag;
